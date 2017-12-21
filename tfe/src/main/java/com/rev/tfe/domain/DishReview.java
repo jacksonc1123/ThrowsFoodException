79376035bbe7108ff3c3bfbed4540f382246adf6
Cameron Jackson <jackson1123@gmail.com>
2017-12-21 09:22:31 -0500

    updates to beans

--- tfe/src/main/java/com/rev/tfe/domain/DishReview.java
+++ tfe/src/main/java/com/rev/tfe/domain/DishReview.java
@@ -7,6 +7,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
