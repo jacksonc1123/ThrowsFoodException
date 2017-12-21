1c2d83ec3ba8f2e40b183ab0612e0bbf3a2a386d
BelliousNick <nick.bell.001@gmail.com>
2017-12-21 12:05:01 -0500

    Update
    User Bean

--- tfe/src/main/java/com/rev/tfe/domain/User.java
+++ tfe/src/main/java/com/rev/tfe/domain/User.java
@@ -1,5 +1,111 @@
 package com.rev.tfe.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+@Table(name="USERS")
 public class User {
 
+	@Id
+	@SequenceGenerator(allocationSize=1,name="U_SEQ",sequenceName="U_ID_SEQ")
+	@GeneratedValue(generator="U_SEQ", strategy=GenerationType.SEQUENCE)
+	private Integer id;
+	
+	@Column(name="USERNAME", nullable=false, unique=true)
+	private String userName;
+	
+	@Column(name="PASSWORD", nullable=false)
+	private String password;
+	
+	@Column(name="FIRSTNAME")
+	private String firstName;
+	
+	@Column(name="LASTNAME")
+	private String lastName;
+	
+	@Column(name="EMAIL", nullable=false, unique=true)
+	private String email;
+	
+	@Column(name="ROLE", nullable=false)
+	private Integer role;
+
+	public User() {}
+	
+	public User(Integer id, String userName, String password, String firstName, String lastName, String email,
+			Integer role) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.role = role;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getRole() {
+		return role;
+	}
+
+	public void setRole(Integer role) {
+		this.role = role;
+	}
+	
 }
