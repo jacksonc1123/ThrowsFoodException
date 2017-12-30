80a0489028473f74c83ffe73b5c805a5dc0e4ed7
Cameron Jackson <jackson1123@gmail.com>
2017-12-29 20:06:28 -0500

    added auth guard

--- project-angular/src/app/app-routing/app-routing.module.ts
+++ project-angular/src/app/app-routing/app-routing.module.ts
@@ -4,12 +4,13 @@ import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
 import { ConfirmProfileComponent } from '../confirm-profile/confirm-profile.component';
 import { UpdateProfileComponent } from '../update-profile/update-profile.component';
 import { DummyPageComponent } from '../dummy-page/dummy-page.component';
+import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dummy-page', pathMatch: 'full' },
   { path: 'dummy-page', component: DummyPageComponent },
   { path: 'confirm-profile', component: ConfirmProfileComponent },
-  { path: 'update-profile', component: UpdateProfileComponent }
+  { path: 'update-profile', component: UpdateProfileComponent, canActivate: [AuthGuard] }
 ]
 
 @NgModule({
