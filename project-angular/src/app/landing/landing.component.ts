106870d8c6980c487732d141256eccf4afa260a0
Cameron Jackson <jackson1123@gmail.com>
2018-01-03 11:46:23 -0500

    commented the deals section

--- project-angular/src/app/landing/landing.component.ts
+++ project-angular/src/app/landing/landing.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  aboutUs: string = `We've been in the pizza making game for years. No other pizza parlor can compare to us. Come on down and see for yourself,
+  the marvels of the Pizza Joint.`
+
   constructor() { }
 
   ngOnInit() {
