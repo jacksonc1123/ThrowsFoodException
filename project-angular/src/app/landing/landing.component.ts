5171e11f55ea5dcdc51c58096ee6a72d465b031a
Cameron Jackson <jackson1123@gmail.com>
2018-01-03 11:55:09 -0500

    removed hardcoding of landing fields

--- project-angular/src/app/landing/landing.component.ts
+++ project-angular/src/app/landing/landing.component.ts
@@ -10,6 +10,18 @@ export class LandingComponent implements OnInit {
   aboutUs: string = `We've been in the pizza making game for years. No other pizza parlor can compare to us. Come on down and see for yourself,
   the marvels of the Pizza Joint.`
 
+  phone: string = '1-800-GET-FOOD';
+  address: string = 'Address: 2714 E Bearss Ave Tampa, FL 33613';
+  operation = [
+    {day: "Sunday", hours: "5pm - 10pm"},
+    {day: "Monday", hours: "5pm - 10pm"},
+    {day: "Tuesday", hours: "5pm - 10pm"},
+    {day: "Wednesday", hours: "5pm - 10pm"},
+    {day: "Thursday", hours: "5pm - 10pm"},
+    {day: "Friday", hours: "5pm - 10pm"},
+    {day: "Saturday", hours: "5pm - 10pm"}
+  ]
+
   constructor() { }
 
   ngOnInit() {
