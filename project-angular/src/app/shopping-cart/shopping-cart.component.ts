0ed155e3d86a8f182b268ce5bbad572af6bb14bf
Shujun Ye <shujunye94@gmail.com>
2018-01-02 19:17:24 -0500

    integrate shopping cart with menu

--- project-angular/src/app/shopping-cart/shopping-cart.component.ts
+++ project-angular/src/app/shopping-cart/shopping-cart.component.ts
@@ -17,75 +17,16 @@ export class ShoppingCartComponent implements OnInit {
   private ticketLines: TicketLine[];
   private ticket: Ticket = new Ticket; // new ticket
   private procTicketline: TicketLine = new TicketLine; // process each ticketline
-  private currentUser: User; // currentUser
+  private currentUser: User;
   private totalDishes: number;
   private totalPrice: number;
-
-  // for testing purpose
-  mockTicketLines: TicketLine[] = [
-    {
-      ticketLineId: 0,
-      quantity: 2,
-      dish: {
-          id: 1,
-          name: "Tomato Garlic Pie",
-          price: 9,
-          description: "marinara no cheese san marzano tomatoes, organic herbs, fresh sliced garlic, extra virgin olive oil"
-      }, 
-      ticket: {
-          ticketId: 0,
-          timeResolved: null,
-          timeSubmitted: null,
-          total: 0,
-          user: {
-              id: 3,
-              userName: "test3",
-              password: "123",
-              email: "c@email.com",
-              firstName: "test3",
-              lastName: "test3",
-              role: 1
-          }
-      }
-  },
-  
-  {
-      ticketLineId: 0,
-      quantity: 1,
-      dish: {
-          id: 5,
-          name: "Bianca",
-          price: 12,
-          description: "original white pie house made fresh mozzarella, hand dipped ricotta, extra virgin olive oil"
-      }, 
-      ticket: {
-          ticketId: 0,
-          timeResolved: null,
-          timeSubmitted: null,
-          total: 0,
-          user: {
-              id: 3,
-              userName: "test3",
-              password: "123",
-              email: "c@email.com",
-              firstName: "test3",
-              lastName: "test3",
-              role: 1
-          }
-          
-      } 
-    }
-  ];
 
   constructor(private router: Router, private ticketService: TicketService, private ticketlineService: TicketlineService) { }
 
   ngOnInit() {
-    // this.ticketLines = JSON.parse(localStorage.getItem("currentTicketlines"));
-    // this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
-    // NEED TO CHANGE AFTER INTEGRATE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    if(this.mockTicketLines){
-      this.ticketLines = this.mockTicketLines;
-      this.currentUser = this.ticketLines[0].ticket.user;
+    this.ticketLines = JSON.parse(localStorage.getItem("shoppingCart"));
+    this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    if(this.ticketLines){
       this.ticketlinesInfo();
     }  
   }
@@ -93,13 +34,12 @@ export class ShoppingCartComponent implements OnInit {
   reset() {
     this.ticketLines.splice(0, this.ticketLines.length);
     this.ticketlinesInfo();
-    localStorage.removeItem("currentTicketlines");
+    localStorage.removeItem("shoppingCart");
   }
 
   ticketlinesInfo() {
     if(this.ticketLines) {
-      // local storage key may be differ
-      localStorage.setItem("currentTicketlines", JSON.stringify(this.ticketLines));
+      localStorage.setItem("shoppingCart", JSON.stringify(this.ticketLines));
       this.totalDishes = 0;
       this.totalPrice = 0;
       for (var ticketline of this.ticketLines){
